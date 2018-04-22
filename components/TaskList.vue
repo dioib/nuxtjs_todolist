269766b3b1e478de59f3951d5f058b4081fcfe4e
dioib <dioib@hotmail.co.jp>
2018-04-22 15:27:52 +0900

    fix timeline css

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,11 +1,11 @@
 <template lang="html">
   <div class="timeline">
       <ul>
-        <li v-for="(post, index) in posts" :key="post['.key']">
-          <div class="hoge" v-if="!post.edit" v-text="post.text"></div>
-          <input v-bind:class="['hoge-' + index]" v-if="post.edit" @focus="focustest()" v-model="post.text">
-          <button @click="editPost(post,index)">{{post.btnEdit ? post.btnEdit : '編集'}}</button>
-          <button @click="removeTodo(post)">削除</button>
+        <li class="timeline_item" v-for="(post, index) in posts" :key="post['.key']">
+          <div class="timeline_text" v-if="!post.edit" v-text="post.text"></div>
+          <input class="timeline_input" v-if="post.edit" @focus="focustest()" v-model="post.text">
+          <button class="timeline_btnEdit" @click="editPost(post,index)">{{post.btnEdit ? post.btnEdit : '編集'}}</button>
+          <button class="timeline_btnRemove" @click="removeTodo(post)">削除</button>
         </li>
       </ul>
       <form @submit.prevent="addToPost">
@@ -30,7 +30,7 @@ export default{
     }
   },
   methods:{
-    removeTodo (post) {
+    async removeTodo (post) {
       this.$store.dispatch('REMOVE_POST', { post })
     },
     async addToPost () {
@@ -51,7 +51,7 @@ export default{
       this.$set(post,"edit", false)
       this.$store.dispatch('UPDATE_POST', { post })
       this.$set(post,"btnEdit","編集")
-    }    
+    }
   },
   created () {
     this.$store.dispatch('INIT_POSTS', this.source)
@@ -65,13 +65,26 @@ export default{
 }
 .timeline .posts{
   height: calc(100% - 36px);
-  overflow-y: auto;
+  
 }
 .post-enter-active {
   animation: slideIn 0.3s linear;
 }
-.hoge{
-  display:inline
+.timeline_item{
+text-align:left;
+}
+.timeline_text{
+display:inline-block;
+width:350pt;
+}
+.timeline_input{
+
+}
+.timeline_btnEdit{
+  margin-right: auto;
+}
+.timeline_btnRemove{
+
 }
 @keyframes slideIn {
   0% {opacity: 0.0}
